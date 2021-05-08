6a6a8d5b78f23e62731eec1243fa7ac0858833cb
virginiagilliland <70280804+virginiagilliland@users.noreply.github.com>
2021-05-08 11:22:02 -0400

    working hardwire haversine condensed

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,17 +3,13 @@
 "use strict";
 //functions to get data inputted by user//
 
-//uh oh itll need to go through every single park and calculate the distance//
-//and then remember all of those distances//
-//and then report back the smallest distance//
-
 //sort array by property
 //console.log(data[0]); //take spaces out of names//
 
 
 // Haversine formula, call convertToMiles(earthRadiusKM, userLat, userLong, parkLat, parkLong) to get final value in miles//
 
-//CHANGING TO ONE FUNCTION//
+//CHANGING TO ONE FUNCTION _-------------------> SEE BELOW FOR CODE //
 /* from jsfiddle today
 function haversineFormula(userLat, userLong, parkLat, parkLong) {
   var radUserLat = userLat * (Math.PI / 180);
@@ -32,15 +28,37 @@ insert convert to miles formula
 
 	return haversine value
 }
-
-console.log(haversineFormula(userLat, userLong, parkLat, parkLong))
 */
-var earthRadiusKM = 6378;
 var userLat = 35.50071444;
 var userLong = -80.84472615;
 var parkLat = 34.67214084;
 var parkLong = -77.14148631;
+//FUNCTION NOW WORKS IN THE CONSOLE!!!!!!!!!!!!!!
+function haversineFormula(userLat, userLong, parkLat, parkLong) {
+  var radUserLat = userLat * (Math.PI / 180);
+  var radUserLong = userLong * (Math.PI / 180);
+  var radParkLat = parkLat * (Math.PI / 180);
+  var radParkLong = parkLong * (Math.PI / 180);
 
+  var difLat = radParkLat - radUserLat;
+  var difLong = radParkLong - radUserLong;
+
+  var haversineOne = Math.sin(difLat/2) * Math.sin(difLat/2) + Math.cos(radUserLat)*Math.cos(radParkLat) * Math.sin(difLong/2) * Math.sin(difLong/2);
+
+  var haversineTwo = 2 * 6378 * Math.asin(Math.sqrt(haversineOne));
+
+  var havmiles = haversineTwo * 0.621371;
+
+	return havmiles;
+}
+
+//console.log(haversineFormula(userLat, userLong, parkLat, parkLong))
+/*
+var userLat = 35.50071444;
+var userLong = -80.84472615;
+var parkLat = 34.67214084;
+var parkLong = -77.14148631;
+/*
 function userLatToRadians(userLat){
   return userLat * (Math.PI / 180);
 }
